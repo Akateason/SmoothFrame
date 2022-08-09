f5758331d75a35f79edf713a6b3b428a6e7c7972
mamba24xtc <akateason@qq.com>
2022-08-10 00:18:55 +0800

    demo [wip] relation to other

--- SmoothFrame/ViewController.swift
+++ SmoothFrame/ViewController.swift
@@ -22,21 +22,25 @@ class ViewController: UIViewController {
         super.viewWillLayoutSubviews()
         
         // relation with super
-        pinkView.sf.setLeft(0).setHeight(99).setBottom(view.sf.safeAreaBottomGap).setRight(3) //
-//        pinkView.sf.setLeft(0).setBottom(view.sf.safeAreaBottomGap).setRight(3).setHeight(99) // 这个有bug, 调用顺序的bug, 因为frame会覆盖
+        pinkView.sf.setLeft(0).setHeight(99).setBottom(0).setRight(3)
         
         // set edges
-        pinkSubView.sf.setEdges(bottom: 23, left: 11, right: 50) // 完美
-        
-        yellowView.sf.setTop(88).setWidth(300).setHeight(88).setCenterXEqual(to: view)
+        pinkSubView.sf.setEdges(bottom: 23, left: 11, right: 50)
         
         // relation with other
-//        greenView.sf.setWidth(200).setHeight(42).setRightEqual(to: yellowView, offset: 21).setTopEqual(to: yellowView, on: .bottom, offset: 14)
-//        greenView.sf.setWidth(200).setHeight(42).setRightEqual(to: yellowView, offset: 21).setTopEqual(to: yellowView, on: .top, offset: 14)
-//        greenView.sf.setWidth(200).setHeight(42).setRightEqual(to: yellowView, offset: 21).setBottomEqual(to: yellowView, on: .bottom, offset: 10)
-//        greenView.sf.setWidth(200).setHeight(42).setRightEqual(to: yellowView, offset: 21).setBottomEqual(to: yellowView, on: .top, offset: 10)
-//        greenView.sf.setWidth(200).setHeight(42).setRightEqual(to: yellowView, on: .left, offset: 22).setBottomEqual(to: yellowView, on: .top, offset: 10)
-        greenView.sf.setWidth(200).setHeight(42).setLeftEqual(to: yellowView, on: .right, offset: 22).setBottomEqual(to: yellowView, on: .top, offset: 10)     
+        yellowView.sf.setTop(88).setWidth(300).setHeight(88).setCenterXEqual(to: view)
+        print("yellow: \(yellowView.frame)")
+        
+        // 有bug 长宽没了, 不明原因 加了pinkView之后, 就没了
+//        greenView.sf
+//            .setTopEqual(to: yellowView, offset: 9)
+//            .setBottomEqual(to: yellowView, on: .bottom, offset: -10)
+//            .setLeftEqual(to: yellowView, on: .left, offset: 22)
+//            .setRightEqual(to: yellowView)
+                
+//        greenView.sf.setTop(33).setLeft(33).setBottom(33).setRight(34)
+        print("green: \(greenView.frame)")
+        print("---------\n")
     }
 
     lazy var pinkView : UIView = {
